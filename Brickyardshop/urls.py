d9a3d5ad864f7b0025be51e1fecbdbe19e2603b8
MFLind <Madde@quaggatech.com>
2023-05-17 06:18:11 +0000

    Creating base html structure

--- Brickyardshop/urls.py
+++ Brickyardshop/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path("", views.index, name="home"),
+    path("webshop", views.webshop, name="webshop"),
+    path("contact", views.contact, name="contact"),
+    path("aboutus", views.aboutus, name="aboutus"),
+    path("gallery", views.gallery, name="gallery"),
 ]
--- Brickyardshop/views.py
+++ Brickyardshop/views.py
@@ -4,6 +4,30 @@ from django.shortcuts import render
 
 
 def index(request):
-    """ A view to return the index page """
+    """A view to return the index page"""
 
-    return render(request, 'home/index.html')
+    return render(request, "home/index.html")
+
+
+def webshop(request):
+    """A view to return the webshop page"""
+
+    return render(request, "home/webshop.html")
+
+
+def contact(request):
+    """A view to return the contact page"""
+
+    return render(request, "home/contact.html")
+
+
+def aboutus(request):
+    """A view to return the aboutus page"""
+
+    return render(request, "home/aboutus.html")
+
+
+def gallery(request):
+    """A view to return the gallery page"""
+
+    return render(request, "home/gallery.html")
