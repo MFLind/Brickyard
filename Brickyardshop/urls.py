e8d01073284708222830663400bd81958793f2e2
MFLind <Madde@quaggatech.com>
2023-05-26 14:56:20 +0000

    Change url path to products app for webshop part

--- Brickyardshop/urls.py
+++ Brickyardshop/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="home"),
-    path("webshop", views.webshop, name="webshop"),
     path("contact", views.contact, name="contact"),
     path("aboutus", views.aboutus, name="aboutus"),
     path("gallery", views.gallery, name="gallery"),
--- Brickyardshop/views.py
+++ Brickyardshop/views.py
@@ -9,17 +9,7 @@ def index(request):
     """A view to return the index page"""
 
     return render(request, "home/index.html")
-
-
-def webshop(request):
-    """A view to return the webshop page"""
-
-    context = {
-        "categories": Category.objects.filter().order_by("friendly_name"),
-        "products": Product.objects.filter(category=0).order_by("name"),
-    }
-    return render(request, "home/webshop.html", context)
-
+    
 
 def contact(request):
     """A view to return the contact page"""
