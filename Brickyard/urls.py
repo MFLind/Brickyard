7a48e45aebd52004acd73491ac188c33bda33d82
MFLind <Madde@quaggatech.com>
2023-05-26 21:35:02 +0000

    make media directory and moved files

--- Brickyard/urls.py
+++ Brickyard/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('basket/', include('basket.urls')),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
