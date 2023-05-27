8166ad8fadcb071e102678c4b25ccc300a03ed86
MFLind <Madde@quaggatech.com>
2023-05-27 20:07:45 +0000

    Integrate code base code from boutiquo ado material

--- Brickyard/settings.py
+++ Brickyard/settings.py
@@ -61,6 +61,8 @@ INSTALLED_APPS = [
     "Brickyardshop",
     "products",
     "basket",
+    "checkout",
+    "profiles",
     # Other
     "crispy_forms",
 ]
@@ -198,8 +200,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
-STRIPE_CURRENCY = "usd"
+STRIPE_CURRENCY = "sek"
 STRIPE_PUBLIC_KEY = os.getenv("STRIPE_PUBLIC_KEY", "")
 STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY", "")
 STRIPE_WH_SECRET = os.getenv("STRIPE_WH_SECRET", "")
-DEFAULT_FROM_EMAIL = "boutiqueado@example.com"
+DEFAULT_FROM_EMAIL = "hej@brickyard.se"
