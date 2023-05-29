362f2ba7989161b51ba47f77047189d89a636d77
MFLind <Madde@quaggatech.com>
2023-05-29 19:35:11 +0000

    Add Heroku instance to allowed host

--- Brickyard/settings.py
+++ Brickyard/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['brickyard.herokuapp.com']
 
 MESSAGE_TAGS = {
     messages.DEBUG: "alert-info",
