d45b99231aaf2af0134dfe009815fa6807cb8cdf
MFLind <Madde@quaggatech.com>
2023-06-19 06:43:26 +0000

    Fixed sending order confirmation via email

--- checkout/views.py
+++ checkout/views.py
@@ -9,6 +9,9 @@ from products.models import Product
 from profiles.models import UserProfile
 from profiles.forms import UserProfileForm
 from basket.contexts import basket_contents
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+
 
 from .forms import OrderForm
 from .models import Order, OrderLineItem
@@ -185,6 +188,18 @@ def checkout_success(request, order_number):
             user_profile_form = UserProfileForm(profile_data, instance=profile)
             if user_profile_form.is_valid():
                 user_profile_form.save()
+
+    cust_email = order.email
+    subject = render_to_string(
+            "checkout/confirmation_emails/confirmation_email_subject.txt",
+            {"order": order},
+    )
+    body = render_to_string(
+        "checkout/confirmation_emails/confirmation_email_body.txt",
+        {"order": order, "contact_email": settings.DEFAULT_FROM_EMAIL},
+    )
+
+    send_mail(subject, body, settings.DEFAULT_FROM_EMAIL, [cust_email])
 
     messages.success(
         request,
